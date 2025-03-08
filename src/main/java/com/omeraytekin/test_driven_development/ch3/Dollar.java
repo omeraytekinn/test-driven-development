2d5be213e98ae3cec380c0545e59f3213d29b044
Ömer Aytekin <o.niketya@gmail.com>
2025-03-08 07:00:59 +0300

    Chapter 3 Equality for All

---
+++ src/main/java/com/omeraytekin/test_driven_development/ch3/Dollar.java
@@ -0,0 +1,19 @@
+package com.omeraytekin.test_driven_development.ch3;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+}
