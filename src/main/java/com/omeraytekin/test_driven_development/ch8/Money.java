bbcde01cf1f590dc97921a69556aa238625a824d
Ömer Aytekin <o.niketya@gmail.com>
2025-03-13 15:10:40 +0300

    Chapter 8 Makin’ Objects

---
+++ src/main/java/com/omeraytekin/test_driven_development/ch8/Money.java
@@ -0,0 +1,21 @@
+package com.omeraytekin.test_driven_development.ch8;
+
+public abstract class Money {
+    protected int amount;
+
+    abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+}
