4529fde06b5483159b6b6615352e1c18b8c6d8b2
Ömer Aytekin <o.niketya@gmail.com>
2025-03-13 18:05:46 +0300

    Chapter 12 Addition, Finally

---
+++ src/main/java/com/omeraytekin/test_driven_development/ch12/Bank.java
@@ -0,0 +1,7 @@
+package com.omeraytekin.test_driven_development.ch12;
+
+public class Bank {
+    public Money reduce(Expression source, String to) {
+        return Money.dollar(10);
+    }
+}
---
+++ src/main/java/com/omeraytekin/test_driven_development/ch12/Expression.java
@@ -0,0 +1,5 @@
+package com.omeraytekin.test_driven_development.ch12;
+
+public interface Expression {
+
+}
